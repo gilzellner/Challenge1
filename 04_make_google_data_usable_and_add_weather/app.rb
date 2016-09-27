dd57b29ec5e3c94a18b9f6813d51c33faaef199e
gilzellner <gil.zellner@gmail.com>
2016-09-27 12:50:35 +0300

    master

--- 04_make_google_data_usable_and_add_weather/app.rb
+++ 04_make_google_data_usable_and_add_weather/app.rb
@@ -22,18 +22,22 @@ def get_weather_data_for_travel(lat, lon)
   return response_body
 end
 
+def get_request_metadata()
+  return "{ source: #{params[:source]},\n
+            destination: #{params[:destination]},\n
+            mintemp: #{params[:mintemp]},\n
+            maxtemp: #{params[:maxtemp]},\n
+            maxtime: #{params[:maxtime]},\n }"
+end
 
 class ShowRequest < Sinatra::Base
   get '/?:source?/?:destination?/?:mintemp?/?:maxtemp?/?:maxtime?' do
     google_api_data = get_google_data_for_travel(params[:source], params[:destination])
-    weather_source = get_weather_data_for_travel(0,0)
-    weather_dest = get_weather_data_for_travel(0,0)
-    json_string = google_api_data.to_json.dup.encode("UTF-8")
-    encoded_hash = JSON.parse(json_string).with_indifferent_access
-
-    "source: #{params[:source]}\n destination: #{params[:destination]}\n
-      mintemp: #{params[:mintemp]}\n maxtemp: #{params[:maxtemp]}\n maxtime: #{params[:maxtime]}\n
-      \nGoogle API data: #{encoded_hash} \nWeather at source: #{weather_source} \nWeather at destination: #{weather_dest}"
+    weather_data = get_weather_data_for_travel(0,0)
+    request_data = get_request_metadata()
+    "{ 'request_data': #{request_data},
+       'google_api_data': #{google_api_data},
+       'weather_data': #{weather_data} }"
   end
 end
 
