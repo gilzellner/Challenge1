10671c866a614af33b06691e45e972a8f509cec9
gilzellner <gil.zellner@gmail.com>
2016-10-05 19:07:22 +0300

    master

--- 05_rework_data/app.rb
+++ 05_rework_data/app.rb
@@ -46,13 +46,17 @@ end
 def get_steps(metadata)
   steps=get_nested_hash_value(metadata, 'steps')
   result = []
-  steps.each { |step| result << { "distance": step["distance"]["text"],
-                                  "end_location": step["end_location"],
-                                  "html_instructions":step["html_instructions"],
-                                  "weather": {
-                                      get_weather_data_for_travel(step["end_location"]["lat"],
-                                                                  step["end_location"]["lng"])
-                                  }} }
+  steps.each do |step|
+    weatherdata = get_weather_data_for_travel(step["end_location"]["lat"],
+                                              step["end_location"]["lng"])
+    result << { "distance": step["distance"]["text"],
+                "end_location": step["end_location"],
+                "html_instructions":step["html_instructions"],
+                "weather":
+                    {"description": weatherdata["weather"][0]["description"],
+                     "celsiusTemp": weatherdata["main"]["temp"]
+                    }}
+  end
   return result
 end
 
@@ -87,25 +91,19 @@ end
 def get_response(metadata)
   openstruct_metadata = RecursiveOpenStruct.new(metadata)
   {'origin': metadata[:request_data][:origin],
-   'destination': metadata[:request_data][:origin],
-   'totalDurationInMinutes':metadata[:google_api_data],
+   'destination': metadata[:request_data][:destination],
+   'totalDurationInMinutes':get_nested_hash_value(metadata,"legs")[0]["duration"],
    'totalDistanceInMeters':openstruct_metadata.google_api_data["routes"][0]["legs"][0]["distance"]["value"],
-   'steps': get_nested_hash_value(metadata, 'steps'),
-   'html_instructions': '',
-   'weather': {'celsiusTemp': 25.3, 'description': 'scattered clouds'},
-   'duration': '8 mins',
-        'end_location': {'lat': 45.5101458, 'lng': -73.5525249},
-   'html_instructions': 'Turn right onto Rue Bonsecours',
-    'weather': {'celsiusTemp': -45.3, 'description': 'Not fun'},
-    'travelAdvice': is_all_weather_between_temps?(get_all_weather_for_data(metadata),metadata[:request_data][:mintemp],metadata[:request_data][:maxtemp]),
-    'metadata':metadata}
+   'steps': get_steps(metadata),
+   'travelAdvice': is_all_weather_between_temps?(get_all_weather_for_data(metadata),metadata[:request_data][:mintemp],metadata[:request_data][:maxtemp])
+  }
 end
 
 class ShowRequest < Sinatra::Base
   get '/?:origin?/?:destination?/?:mintemp?/?:maxtemp?/?:maxtime?' do
     metadata=get_request_metadata()
     JSON.pretty_generate(get_response(metadata))
-    byebug
+    # byebug
   end
 end
 
